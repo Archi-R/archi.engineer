5e5518d70c99a8ab25a8ad06ebbabf9f26b4728b
Ronan PEYREL <archib71@gmail.com>
2023-10-16 21:08:36 +0200

    1ere mise en prod

---
+++ resources/js/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>Bienvenue sur mon site !</h1>
+        <p>Ceci est ma première page avec InertiaJS et Laravel.</p>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
