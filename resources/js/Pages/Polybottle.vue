e2a7761166c75bd55d8077606c4811f4d63ca35b
Ronan PEYREL <archib71@gmail.com>
2023-11-24 14:08:00 +0100

    amélioration

--- resources/js/Pages/Polybottle.vue
+++ resources/js/Pages/Polybottle.vue
@@ -1,8 +1,8 @@
 <!-- PromoPage.vue -->
 <template>
-  <div style="background: linear-gradient(120deg, #a8ff78, #78ffd6); padding: 20px;">
-    <q-card style="max-width: 400px; margin: auto; padding: 20px; border-radius: 8px; box-shadow: 0 4px 6px rgba(0,0,0,0.1);">
-      <h1 style="text-align: center; color: #333; font-size: xxx-large">Buy the PolyBottle</h1>
+    <div style="background: linear-gradient(120deg, #a8ff78, #78ffd6); padding: 20px;">
+        <q-card style="max-width: 400px; margin: auto; padding: 20px; border-radius: 8px; box-shadow: 0 4px 6px rgba(0,0,0,0.1);">
+            <h1 style="text-align: center; color: #333; font-size: 6vw">Buy the PolyBottle</h1>
       <form>
         <div class="q-mb-md">
           <label for="name" style="color: #555;">Name:</label>
@@ -15,7 +15,7 @@
         <q-btn label="Pay" type="submit" color="accent" class="full-width" />
       </form>
     </q-card>
-    <img src="../../images/polybottle.png" height="771" width="460" style="display: block; margin: 20px auto;" alt="bouteille stylax"/>
+    <img src="../../images/polybottle.png" style="max-width: 100%; height: auto; display: block; margin: 20px auto;" alt="bouteille stylax"/>
     <div class="q-mb-md" style="text-align: center;"
     >Site by Archi (Ronan PEYREL)</div>
   </div>
